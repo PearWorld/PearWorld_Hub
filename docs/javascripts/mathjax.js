1dcdde3f70ec5f8dbe34b420f0881a3ecc151d00
PearWorld <PearWorld1235@gmail.com>
2026-01-25 12:58:11 +0800

    init

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,7 +1,7 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["$", "$"], ["\\(", "\\)"]],
-    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
     processEscapes: true,
     processEnvironments: true
   },
@@ -11,6 +11,15 @@ window.MathJax = {
   }
 };
 
+/* ==========================================================================
+   关键修复：适配 Instant Navigation (瞬间加载)
+   ========================================================================== */
 document$.subscribe(() => { 
+  // 1. 重新渲染新页面上的公式
   MathJax.typesetPromise()
+  
+  // 2. 额外配置：让 MathJax 也能识别单个 $ 符号 (Typora 习惯)
+  // 虽然 arithmatex 扩展默认转换了，但为了保险，我们加上这个
+  window.MathJax.tex.inlineMath.push(['$', '$']);
+  window.MathJax.tex.displayMath.push(['$$', '$$']);
 })
